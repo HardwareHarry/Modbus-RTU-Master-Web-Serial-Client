6fa45bb5d74479f99a64171800f3cb3270656737
HardwareHarry <paul@harris.cloud>
2026-03-05 10:35:14 +0000

    updated again for lint v10 format!

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-import globals from "globals";
-
 export default [
   {
     files: ["**/*.html", "**/*.js"],
@@ -7,11 +5,41 @@ export default [
       ecmaVersion: 2022,
       sourceType: "script",
       globals: {
-        ...globals.browser,
+        // Browser environment
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        localStorage: "readonly",
+        sessionStorage: "readonly",
+        console: "readonly",
+        alert: "readonly",
+        confirm: "readonly",
+        prompt: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        performance: "readonly",
+        URL: "readonly",
+        Blob: "readonly",
+        File: "readonly",
+        FileReader: "readonly",
+        FormData: "readonly",
+        Event: "readonly",
+        CustomEvent: "readonly",
+        HTMLElement: "readonly",
+        ArrayBuffer: "readonly",
+        DataView: "readonly",
+        Uint8Array: "readonly",
+        BigInt: "readonly",
+        fetch: "readonly",
+        AbortController: "readonly",
+        TextEncoder: "readonly",
+        TextDecoder: "readonly",
       },
     },
     rules: {
-      // Functions called from HTML onclick attributes appear unused to ESLint
+      // Functions called from HTML onclick/onchange attributes appear unused to ESLint
       "no-unused-vars": ["warn", {
         varsIgnorePattern: "^(toggleConnection|disconnect|toggleSection|saveCurrentProfile|exportProfile|importProfile|loadSaved|delSaved|openProfile|closeProfile|openAbout|closeAbout|sendQuery|setFC|togglePolling|clearLog|addProfileEntry|copyFirstToAll|reRender|clearActiveProfile|updateOffsetUI|updateWirePreview)$"
       }],
