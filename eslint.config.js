eb89d37795f8d949f4bb06f8ba9ba82cb67f244a
HardwareHarry <paul@harris.cloud>
2026-03-05 10:31:31 +0000

    Updated eslint config to support browser objects

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,20 @@
-import html from "eslint-plugin-html";
-import js from "@eslint/js";
+import globals from "globals";
 
 export default [
-    js.configs.recommended,
-    {
-        files: ["**/*.html"],
-        plugins: {
-            html
-        },
-        languageOptions: {
-            globals: {
-                window: "readonly",
-                document: "readonly",
-                console: "readonly"
-            },
-            ecmaVersion: "latest",
-            sourceType: "module"
-        },
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "error"
-        }
-    }
+  {
+    files: ["**/*.html", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      // Functions called from HTML onclick attributes appear unused to ESLint
+      "no-unused-vars": ["warn", {
+        varsIgnorePattern: "^(toggleConnection|disconnect|toggleSection|saveCurrentProfile|exportProfile|importProfile|loadSaved|delSaved|openProfile|closeProfile|openAbout|closeAbout|sendQuery|setFC|togglePolling|clearLog|addProfileEntry|copyFirstToAll|reRender|clearActiveProfile|updateOffsetUI|updateWirePreview)$"
+      }],
+    },
+  },
 ];
